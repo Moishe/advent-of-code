7e205330d9f997ac0ed0c423f5557c49c8d74425
Moishe Lettvin <moishel@gmail.com>
2020-12-19 15:42:28 -0700

    Super messy day 19 rewrite, but it works for part 1

--- day_19/cfg.py
+++ day_19/cfg.py
@@ -10,6 +10,7 @@ input = [
     '4: "a"',
     '5: "b"',
 ]
+
 """
 input = [
     '0: 1 2',
@@ -17,14 +18,18 @@ input = [
     '2: 1 3 | 3 1',
     '3: "b"'
 ]
-"""
-
 input = [
-    '0: 1 2',
-    '1: 3 1 2 | 3 2',
+    '0: 3 1 2 3',
+    '1: 3 1 3 | 2',
     '2: "a"',
     '3: "b"',
 ]
+"""
+
+f = open(pathlib.Path(__file__).parent.absolute() / 'rules.txt')
+input = [x.rstrip() for x in f.readlines()]
+
+
 def array_from_rule(rule):
     rule_array = []
     clauses = rule.split('|')
@@ -32,16 +37,16 @@ def array_from_rule(rule):
         clause_array = []
         elements = clause.split(' ')
         for el in elements:
-            if el:                
+            if el:
                 clause_array.append(eval(el))
         rule_array.append(clause_array)
 
     return rule_array
 
 def expand_rule(rule, rule_idx, rule_map):
-    new_rule = []
+    new_rule = ['or']
     for clause in rule:
-        new_clause = []
+        new_clause = ['and']
         for el in clause:
             if type(el) is int:
                 if el == rule_idx:
@@ -56,47 +61,99 @@ def expand_rule(rule, rule_idx, rule_map):
         return new_rule[0][0]
     return new_rule
 
-def match(s, ruleset, parent, depth=0):
-    print("%s Trying to match %s to %s" % (' ' * depth, s, ruleset))
-    idx = 0
+def debug_print(s):
+    #print(s)
+    pass
+
+def match_2(s, idx, ruleset, depth=0):
+    debug_print("%s Matching %s against %s" % (' ' * depth, s[idx:], ruleset))
+
     matches = False
-    if type(ruleset[0]) == list:
-        for el in ruleset:
-            print("%s OR %s / %s" % (' ' * depth, el,s))
-            matches |= match(s[idx:], el, ruleset, depth + 1)
+    local_ruleset = ruleset.copy()
+    operation = local_ruleset.pop(0)
+    if operation == 'or':
+        for el in local_ruleset:
+            debug_print("%s Checking: %s %s" % (' ' * depth, el, s))
+            if type(el) is str:
+                if el == s[idx]:
+                    return (True, idx + 1)
+            elif type(el) is int:
+                print("Okay I dunno now")
+                exit(1)
+            else:
+                (new_match, new_idx) = match_2(s, idx, el, depth + 1)
+                if new_match:
+                    return (True, new_idx)
+        return (matches, idx)
     else:
-        for el in ruleset:
-            print("%s AND %s / %s" % (' ' * depth, el,s[idx]))
+        for el in local_ruleset:
+            if type(el) is str:
+                if not el == s[idx]:
+                    debug_print("%s Matches (%s, %s)" % (' ' * depth, False, el))
+                    return (False, idx)
+                idx += 1
+            elif type(el) is int:
+                print("Okay I dunno now")
+                exit(1)
+                """
+                (new_match, new_idx) = match_2(s, idx, expanded_ruleset[el], depth + 1)
+                if not new_match:
+                    debug_print("%s Matches (%s, %s)" % (' ' * depth, False, el))
+                    return (False, idx)
+                else:
+                    idx = new_idx
 
-            if el == '[!]':
-                print("%s Recursing" % (' ' * depth))
-                if not match(s[idx:], parent, None, depth + 1):
-                    return False
-                #print("felt recursive, might delete later %s, %s, %s" % (m, s[idx:], ruleset))
-                # not sure what to *do* with this
-            elif s[idx] != el:                
-                return False
-            idx += 1
-
-    return matches
+                """
+            else:
+                (new_match, new_idx) = match_2(s, idx, el, depth + 1)
+                if not new_match:
+                    debug_print("%s Matches (%s, %s)" % (' ' * depth, False, el))
+                    return (False, idx)
+                else:
+                    idx = new_idx
+        return (True, idx)
 
 rule_map = {}
-
-#f = open(pathlib.Path(__file__).parent.absolute() / 'rules.txt')
-#input = [x.rstrip() for x in f.readlines()]
 
 for line in input:
     m = re.match(r'^([0-9]+): (.+)$', line)
     (rule_number, rule) = m.groups()
     rule_map[int(rule_number)] = array_from_rule(rule)
 
-print(json.dumps(rule_map, indent=1))
+#print(json.dumps(rule_map, indent=1))
+
+expanded_ruleset = []
+for (idx, rule) in rule_map.items():
+    expanded_ruleset.append(expand_rule(rule, 0, rule_map))
 
 expanded_ruleset = expand_rule(rule_map[0], 0, rule_map)
-print(expanded_ruleset)
 
-print(match('bbaaa', expanded_ruleset, None))
-exit(1)
+#print(json.dumps(expanded_ruleset, indent=1))
+#exit(0)
+
+#print(match_2('baab', 0, expanded_ruleset))
+#exit(1)
+
+#print(match_2('bbaaa', 0, expanded_ruleset))
+#exit(1)
+
+testlines = [
+    'ababbb',
+    'bababa',
+    'abbbab',
+    'aaabbb',
+    'aaaabbb',
+]
 
 f = open(pathlib.Path(__file__).parent.absolute() / 'testlines.txt')
 testlines = [x.rstrip() for x in f.readlines()]
+
+total = 0
+for testline in testlines:
+    (result, idx) = match_2(testline, 0, expanded_ruleset)
+    matches = result and idx == len(testline)
+    if matches:
+        total += 1
+    print("%s: %s (%d, %d)" % (testline, matches, idx, len(testline)))
+
+print("Total: %d" % total)
